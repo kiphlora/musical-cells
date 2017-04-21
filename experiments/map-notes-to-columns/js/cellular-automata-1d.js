3d4e66ef6eab540ce9d9b4ec5e7931b74a5c4eaf
kiphlora <bmoran.bam@gmail.com>
2017-04-20 20:55:33 -0400

    revamp everything
    Create experiments to look at different ways to generate music from ca

--- experiments/map-notes-to-columns/js/cellular-automata-1d.js
+++ experiments/map-notes-to-columns/js/cellular-automata-1d.js
@@ -610,6 +610,14 @@ function sum(arr) {
 	return s;
 }
 
+function vecOp(arr, f) {
+	var a = [];
+	for (var i=0; i<arr.length; i++) {
+		a[i] = f(arr[i], i);
+	}
+	return a;
+}
+
 function areArraysEqual(arr1, arr2) {
 	if (arr1.length !== arr2.length) return false;
 	for (var i=0; i<arr1.length; i++) {
@@ -618,4 +626,26 @@ function areArraysEqual(arr1, arr2) {
 	return true;
 }
 
+function buildArray(len, initVal) {
+	var a = [];
+	for (var i=0; i<len; i++) {
+		a[i] = initVal;
+	}
+	return a;
+}
 
+function buildCustomArray(len, f) {
+	var a = [];
+	for (var i=0; i<len; i++) {
+		a[i] = f(i);
+	}
+	return a;
+}
+
+function max(arr) {
+	var m = [0, arr[0]];
+	for (var i=0; i<arr.length; i++) {
+		m = m[1] >= arr[i] ? m : [i, arr[i]];
+	}
+	return m;
+}
